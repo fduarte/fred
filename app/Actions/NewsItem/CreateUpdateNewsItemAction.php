58e46e29693a677516fcf9cb8bf6dfa4625c9bc0
Fred Duarte <fredduarte@gmail.com>
2025-04-14 18:26:27 +0000

    Refactor to use an Action to CreateUpdate News Item

---
+++ app/Actions/NewsItem/CreateUpdateNewsItemAction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Actions\NewsItem;
+
+use App\Models\NewsItem;
+use Illuminate\Support\Facades\DB;
+
+final class CreateUpdateNewsItemAction {
+
+    public function handle(array $newsItemDtos): void {
+
+        // Save data
+        DB::transaction(function () use ($newsItemDtos) {
+            foreach ($newsItemDtos as $newsItemDto) {
+                NewsItem::updateOrCreate(
+                    ['url' => $newsItemDto['url']],
+                    $newsItemDto
+                );
+            }
+        });
+
+
+        // Broadcast new import
+        // broadcast(new NewsItemCreatedUpdated($newsItemDto))->toOthers();
+
+    }
+}
