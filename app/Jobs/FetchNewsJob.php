2885c4fb79b9382fc1dee7338af3c2f4ad359315
Freddy Duarte <57608+fduarte@users.noreply.github.com>
2025-04-23 14:22:47 +0000

    Handling exceptions in FetchNews job and setting max tries

--- app/Jobs/FetchNewsJob.php
+++ app/Jobs/FetchNewsJob.php
@@ -7,26 +7,36 @@ namespace App\Jobs;
 use App\Actions\News\CreateUpdateNewsItemAction;
 use App\NewsAggregator\Enums\NewsProviderCategory;
 use App\NewsAggregator\Factories\NewsProviderFactory;
+use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use InvalidArgumentException;
+use Throwable;
 
 final class FetchNewsJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    /**
+     * @var int
+     */
+    public int $tries = 5;
+
     public function __construct(public NewsProviderCategory $category) {}
 
     public function handle(CreateUpdateNewsItemAction $createUpdateNewsItemAction): void
     {
-        logger()->info('Fetching news items');
+        logger()->info('Start fetching news...');
 
-        $provider = NewsProviderFactory::make($this->category);
-
-        $createUpdateNewsItemAction->handle($provider->fetch());
-
-        logger()->info('Terminated fetching news items');
+        try {
+            $provider = NewsProviderFactory::make($this->category);
+            $createUpdateNewsItemAction->handle($provider->fetch());
+            logger()->info('Terminated fetching news: ' . $this->category->value);
+        } catch (Exception | Throwable $e) {
+            logger()->error($e->getMessage());
+        }
     }
 }
