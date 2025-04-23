f4eee6b2296f3a73643578d51fa598b9e211a5d7
Freddy Duarte <57608+fduarte@users.noreply.github.com>
2025-04-23 14:26:54 +0000

    Added scope, default date and toDto method

--- app/Models/NewsItem.php
+++ app/Models/NewsItem.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\NewsAggregator\DTOs\NewsItemDto;
 use App\NewsAggregator\Enums\NewsProviderCategory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -26,6 +27,18 @@ final class NewsItem extends Model
         return [
             'category' => NewsProviderCategory::class,
             'tags' => 'array',
+            'published_at' => 'datetime:Y-m-d',
         ];
     }
+
+    public function scopePublished($query)
+    {
+        return $query->where('published', true);
+    }
+
+    public function toDto(): NewsItemDto
+    {
+        return NewsItemDto::from($this->toArray());
+    }
+
 }
